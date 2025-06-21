115bae38fd5167e87c6dc67fd6f143f4c4d3ba3a
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-06-21 10:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const num1 = document.getElementById("n1");
+const num2 = document.getElementById("n2");
+const calculateBtn = document.getElementById("sum_btn");
+const resultP = document.getElementById("sum");
+
+calculateBtn.addEventListener("click", () => {
+	let sum = parseInt(num1.value) + parseInt(num2.value);
+	resultP.textContent = sum;
+})
